b3f36a3bc02b48c5a597b96b20716ae06d3cc583
stawary <15800916230@126.com>
2018-01-25 15:09:35 +0800

    Update Add_Two_Numbers.py

--- LeetCode/Add_Two_Numbers.py
+++ LeetCode/Add_Two_Numbers.py
@@ -1,1 +1,12 @@
+#You are given two non-empty linked lists representing two non-negative integers. The digits are 
+#stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+#给定两个非空链表表示两个整数，这些数字以相反的顺序存储，每个节点都包含一个数字。将两个数字相加并将其作为一个链表返回。
+
+#You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+#Example
+
+#Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+#Output: 7 -> 0 -> 8
+#Explanation: 342 + 465 = 807.
 
