2c5621e03471104fe01997ee399f2cfb4ac453a6
stawary <15800916230@126.com>
2018-01-25 14:45:36 +0800

    Update Two_Sum.py

--- LeetCode/Two_Sum.py
+++ LeetCode/Two_Sum.py
@@ -22,6 +22,24 @@ class Solution:
                     result.append(j)
         return result
       
-#时间复杂度 O(n2)，循环遍历列表所有元素，找到可以配对的值。最蛮力的方法。
+#时间复杂度 O(n2)，循环遍历2次列表元素，找到可以配对的值。最蛮力的方法。
 #python的此种方法如果数量级很大，会时间超限。
 
+#另一种算法，使用哈希算法，python中即dict
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i in range(len(nums)):
+            x = nums[i]
+            if target-x in dic:
+                return [dic[target-x], i]
+            dic[x] = i
+
+#时间复杂度O(n)，只遍历一次列表。利用dict的键值对，num:index，依次写入到dict里。然后借助dict的in操作符，
+#如果键存在于字典里，返回True，用以判断是否存在target-x。如果存在，返回键所对应的值，即索引。
