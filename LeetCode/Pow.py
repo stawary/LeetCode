68744c97e1a5d7698cbacc991712e19486357b90
stawary <15800916230@126.com>
2018-01-27 07:23:40 +0800

    Update Pow.py

--- LeetCode/Pow.py
+++ LeetCode/Pow.py
@@ -1,5 +1,5 @@
 #Implement pow(x, n).
-
+#计算x的n次方
 #Example 1:
 
 #Input: 2.00000, 10
@@ -8,3 +8,14 @@
 
 #Input: 2.10000, 3
 #Output: 9.26100
+
+#第一种传统方法，逐个相乘，需考虑n<0的情况，x=1/x。
+class Solution:
+    def myPow(self, x, n):
+        ans = 1
+        if n<0:
+            x=1/x
+        
+        for i in range(abs(n)):
+            ans = ans*x
+        return ans
