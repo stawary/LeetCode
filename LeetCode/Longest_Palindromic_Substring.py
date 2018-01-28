e0ce7498cf4792cfcebd6175efd18493d5770d93
stawary <15800916230@126.com>
2018-01-28 09:11:21 +0800

    Update Longest_Palindromic_Substring.py

--- LeetCode/Longest_Palindromic_Substring.py
+++ LeetCode/Longest_Palindromic_Substring.py
@@ -1,1 +1,13 @@
+#Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+#给定字符串s，找到s里的最长回文子串
+#Example:
 
+#Input: "babad"
+#Output: "bab"
+
+#Note: "aba" is also a valid answer.
+ 
+#Example:
+
+#Input: "cbbd"
+#Output: "bb"
